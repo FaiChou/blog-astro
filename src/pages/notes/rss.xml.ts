e4b68eb174da23d463157d1faa5e6e1561a215b9
周辉 <faichou.zh@gmail.com>
2026-01-30 10:09:27 +0800

    Revert "feat: add full HTML content to RSS feed items"
    This reverts commit fd223763760c359e5e8df285b7b4e6e32c85709a.

--- src/pages/notes/rss.xml.ts
+++ src/pages/notes/rss.xml.ts
@@ -1,7 +1,6 @@
-import { getCollection, render } from "astro:content";
+import { getCollection } from "astro:content";
 import { siteConfig } from "@/site.config";
 import rss from "@astrojs/rss";
-import { experimental_AstroContainer as AstroContainer } from "astro/container";
 
 export const GET = async () => {
 	const notes = await getCollection("note");
@@ -10,18 +9,10 @@ export const GET = async () => {
 		title: siteConfig.title,
 		description: siteConfig.description,
 		site: import.meta.env.SITE,
-		items: await Promise.all(
-			notes.map(async (note) => {
-				const { Content } = await render(note);
-				const container = await AstroContainer.create();
-				const html = await container.renderToString(Content);
-				return {
-					title: note.data.title,
-					pubDate: note.data.publishDate,
-					link: `notes/${note.id}/`,
-					content: html,
-				};
-			}),
-		),
+		items: notes.map((note) => ({
+			title: note.data.title,
+			pubDate: note.data.publishDate,
+			link: `notes/${note.id}/`,
+		})),
 	});
 };
